6ef6d841e6fc72f5bc37a4a15a54e7e987d1ee3e
Sichem Zhou <xeechou@gmail.com>
2016-03-19 21:55:59 -0400

    dummy server

--- src/raspi/server.js
+++ src/raspi/server.js
@@ -1,8 +1,9 @@
 import Express from 'express';
 import socketIo from 'socket.io';
 import { Server as server } from 'http';
-
+import  gpio from 'rpi-gpio'
 import path from 'path';
+
 
 // Webpack Requirements
 import webpack from 'webpack';
@@ -22,6 +23,11 @@ if (process.env.NODE_ENV !== 'production') {
 // initialize socket.io
 const http = server(app);
 const io = socketIo(http);
+let motorState = false;
+let servoState = false;
+let directState = 1;
+
+
 
 app.use(Express.static(path.join(__dirname, '..', 'public')));
 
@@ -31,20 +37,39 @@ app.get('/', (req, res) => {
 
 http.listen(3000, () => console.log('listening on 3000'));
 
+const MotorPin  = 40
+const servoPin  = 30
+//gpio.setup(MotorPin, gpio.DRI_OUT, write);
+
+
+
 io.on('connection', socket => {
   // turn on/off the motors -> on vs off
-  socket.on('motor:toggle', () => {
-  });
+    socket.on('motor:toggle', (val) => {
+/*	gpio.write(MotorPin, val, function(err) {
+	    if (err) throw err;
+	}); */
+	motorState = val;
+	io.emit('motor:state', motorState);
+    });
 
   // start motion (toggle servo) -> block vs unbloc
-  socket.on('servo:toggle', () => {
+    socket.on('servo:toggle', (val) => {
+/*	gpio.write(servoPin, val, fucntion(err) {
+	    if (err) throw err;
+	}); */
+	servoState = val;
+	io.emit('servo:state', servoState);
   });
 
   // toggle direction -> forward vs reverse
-  socket.on('motor:direction', () => {
+    socket.on('direction:toggle', (val) => {
+	directState = val;
+	io.emit('direction:state', directState);
   });
 
   // listen to motor speed (analog)
-  socket.on('motor:speed', () => {
+    socket.on('motor:speed', () => {
+	
   });
 });
